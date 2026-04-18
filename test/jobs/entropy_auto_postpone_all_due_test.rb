67549daf0fbcd6ccbe5b61b3946a6be27e3c7985
Timm Stokke <timm@stokke.me>
2026-04-17 22:18:33 -0700

    I-S4 (fizzy-858.10): job test for entropy defer actor/until

---
+++ test/jobs/entropy_auto_postpone_all_due_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+class EntropyAutoPostponeAllDueTest < ActiveSupport::TestCase
+  setup do
+    Current.session = sessions(:david)
+    Rails.application.config.x.fizzy.install_hostname = "test.local"
+  end
+
+  test "auto_postpone_all_due defers with system actor and does not leak Current.actor" do
+    freeze_time
+
+    card = cards(:logo)
+    Card.stubs(:due_to_be_postponed).returns(Card.where(id: card.id))
+
+    original_actor = "kevin@example.com"
+    Current.actor = original_actor
+
+    until_time = (Time.current + card.auto_postpone_period.to_i).iso8601
+    status = stub(success?: true, exitstatus: 0)
+
+    Open3.expects(:capture3)
+      .with("bd", "--actor", "system@test.local", "defer", card.id.to_s, "--until=#{until_time}")
+      .returns(["", "", status])
+
+    Card.auto_postpone_all_due
+
+    assert_equal original_actor, Current.actor
+  end
+end
+
